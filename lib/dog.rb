9c1f622f377243d4b71edc629f9858f378fa2fda
Marvin Reyes <reyemarv@gmail.com>
2020-02-16 00:43:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,7 +6,6 @@ class Dog
   
   def initialize(name)
     @name = name 
-    @save
   end 
   
   def self.all
@@ -17,7 +16,7 @@ class Dog
     @@all.each {|x| puts x.name}
   end
   
-  def @save(name)
+  def save(name)
     @@all << name
   end 
 end 
