7c6ad52e1fc5b80908e8070dec9b295f899d7713
Marvin Reyes <reyemarv@gmail.com>
2020-02-16 00:39:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @@all.each {|x| puts x.name}
+  end
+end 
+  
